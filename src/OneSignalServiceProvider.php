9ba522a4be390dbae928455324b6f4fb3b4b9bba
Ahmed Fathy <aliraqi.dev@gmail.com>
2018-11-26 16:26:09 +0200

    add alias for notification channel

--- src/OneSignalServiceProvider.php
+++ src/OneSignalServiceProvider.php
@@ -4,6 +4,7 @@ namespace NotificationChannels\OneSignal;
 
 use Berkayk\OneSignal\OneSignalClient;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Notification;
 use NotificationChannels\OneSignal\Exceptions\InvalidConfiguration;
 
 class OneSignalServiceProvider extends ServiceProvider
@@ -13,20 +14,26 @@ class OneSignalServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $oneSignalClient = new OneSignalClient(
+            config('services.onesignal.app_id'),
+            config('services.onesignal.rest_api_key'),
+            ''
+        );
+
+        Notification::extend('onesignal', function () use ($oneSignalClient) {
+            return new OneSignalChannel($oneSignalClient);
+        });
+
         $this->app->when(OneSignalChannel::class)
             ->needs(OneSignalClient::class)
-            ->give(function () {
-                $oneSignalConfig = config('services.onesignal');
+            ->give(function () use ($oneSignalClient) {
+                $oneSignalConfig = $this->app['config']->get('services.onesignal');
 
                 if (is_null($oneSignalConfig)) {
                     throw InvalidConfiguration::configurationNotSet();
                 }
 
-                return new OneSignalClient(
-                    $oneSignalConfig['app_id'],
-                    $oneSignalConfig['rest_api_key'],
-                    ''
-                );
+                return $oneSignalClient;
             });
     }
 }
